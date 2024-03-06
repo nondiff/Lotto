b604abdf53d31173e41520cf0f9139b792665dc4
nondiff <caici1314520@gmail.com>
2024-03-06 17:47:14 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/shan/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/shan/lotto/MainActivity.kt
@@ -2,15 +2,21 @@ package tw.edu.pu.shan.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv : TextView = findViewById(R.id.txv)
+        txv.text = "0"
     }
 
-    var txv : TextView = findViewById(R.id.txv)
-    txv.text = "0"
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
+    }
 
 }
