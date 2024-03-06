3164598fd7ba16a3af8a624e676d4753e18bf3a4
nondiff <caici1314520@gmail.com>
2024-03-06 16:52:09 +0800

    num.2

--- app/src/main/java/tw/edu/pu/shan/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/shan/lotto/MainActivity.kt
@@ -8,4 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    var txv : textview = findViewById(R.id.txv)
+    txv.text = "0"
+
 }
